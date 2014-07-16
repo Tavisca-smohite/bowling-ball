63de9fc840cc1b73833240ff321ed71c0f6f8ac1
sheetal041292 <smohite@tavisca.com>
2014-07-16 10:12:28 +0530

    first commit

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -8,15 +8,134 @@ namespace Bowling
 {
     public class Game
     {
+        public List<int> FlagsForFrame = new List<int>();
+        public List<List<int>> TrackOfThrows = new List<List<int>>();
+        List<int> throwsPerFrame = new List<int>();
+        
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            MaintainScorePerFrame(pins);
+
+           
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int FinalScore = GetCurrentScore();
+            
+            return FinalScore;
+        }
+        public int GetCurrentScore()
+        {
+            int result = 0;
+            for (int i = 0; i < TrackOfThrows.Count && i < 10; i++)
+            {
+                
+                if (FlagsForFrame[i] == 2)
+                {
+                    result += TrackOfThrows[i][0];
+                    if (i + 1 < TrackOfThrows.Count && FlagsForFrame[i + 1] < 2)
+                    {
+                        for (int k = 0; k < TrackOfThrows[i + 1].Count; k++)
+                        {
+                            result += TrackOfThrows[i + 1][k];
+                        }
+                       
+
+                    }
+                    else if (i + 1 < TrackOfThrows.Count && FlagsForFrame[i + 1] == 2)
+                    {
+                        result += TrackOfThrows[i + 1][0];
+                        if (i + 2 < TrackOfThrows.Count)
+                        {
+                            result += TrackOfThrows[i + 2][0];
+                        }
+                        
+                    }
+                }
+                else if (FlagsForFrame[i] == 1)
+                {
+                    for (int k = 0; k < TrackOfThrows[i].Count; k++)
+                    {
+                        result += TrackOfThrows[i][k];
+                    }
+                    if (i + 1 < TrackOfThrows.Count)
+                    {
+                        result += TrackOfThrows[i + 1][0];
+                    }
+                   
+                }
+                else if (FlagsForFrame[i] == 0)
+                {
+                    for (int k = 0; k < TrackOfThrows[i].Count; k++)
+                    {
+                        result += TrackOfThrows[i][k];
+                    }
+                }
+
+
+               
+
+
+            }
+
+            return result;
         }
 
+        public void MaintainScorePerFrame(int pins)
+        {
+
+            
+            if (throwsPerFrame.Count < 2)
+            {
+                if (throwsPerFrame.Count == 0)
+                {
+                    
+                    throwsPerFrame.Add(pins);
+                    if (pins == 10)
+                    {
+                        FlagsForFrame.Add(2);
+
+                        var temp = new List<int>();
+                        foreach (var item in throwsPerFrame)
+                            temp.Add(item);
+                        TrackOfThrows.Add(temp);
+
+                        throwsPerFrame.Clear();
+                    }
+                    else
+                    {
+                        FlagsForFrame.Add(0);
+                        TrackOfThrows.Add(throwsPerFrame);
+                    }
+                }
+                else if (throwsPerFrame.Count == 1)
+                {
+                    throwsPerFrame.Add(pins);
+                    int result = throwsPerFrame.Aggregate((a, b) => b + a);
+                    if (result == 10)
+                    {
+                        FlagsForFrame[FlagsForFrame.Count - 1] = 1; //spare
+
+                    }
+                    
+                    var temp = new List<int>();
+                    foreach (var item in throwsPerFrame)
+                        temp.Add(item);
+                    TrackOfThrows[TrackOfThrows.Count - 1] = temp;
+                    throwsPerFrame.Clear();
+                }
+            }
+            else
+            {
+                throwsPerFrame.Clear();
+            }
+
+
+        }
+
+
     }
+
 }
+
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BowlingFixtures
@@ -7,8 +8,133 @@ namespace BowlingFixtures
     public class GameFixture
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TotalFrameCountShouldBeLessThanOrEqualToTen()
         {
+
+            var obj = new Bowling.Game();
+            for (int i = 0; i < 10; i++)
+            {
+                obj.Roll(7);
+                obj.Roll(3);
+                int track = obj.TrackOfThrows[i].Aggregate((a, b) => b + a);
+                if (track > 10)
+                {
+                    Assert.Fail("invalid inputs: total pins in a frame cant be more than 10");
+                }
+            }
+            
         }
+
+        [TestMethod]
+        public void AllStrikes()
+        {
+
+            var obj = new Bowling.Game();
+            int finalscore = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                obj.Roll(10);
+                finalscore = obj.GetScore();
+            }
+            if (finalscore != 300)
+            {
+                Assert.Fail("test failed for all strikes condition");
+            }
+
+
+        }
+
+        [TestMethod]
+        public void AllGutters()
+        {
+
+            var obj = new Bowling.Game();
+            int FinalScore = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                obj.Roll(0);
+                FinalScore = obj.GetScore();
+            }
+            if (FinalScore != 0)
+            {
+                Assert.Fail("test failed for all gutter condition");
+            }
+
+        }
+
+        [TestMethod]
+        public void AllSpares()
+        {
+
+            var obj = new Bowling.Game();
+            int FinalScore = 0;
+            for (int i = 0; i < 21; i++)
+            {
+                obj.Roll(5);
+                FinalScore = obj.GetScore();
+            }
+            if (FinalScore != 150)
+            {
+                Assert.Fail("test failed for all spares condition");
+            }
+
+        }
+
+        [TestMethod]
+        public void Mixture()
+        {
+
+            var obj = new Bowling.Game();
+            int FinalScore = 0;
+            obj.Roll(5); obj.Roll(5);
+            obj.Roll(4); obj.Roll(3);
+            obj.Roll(10);
+            obj.Roll(2); obj.Roll(3);
+            obj.Roll(0); obj.Roll(8);
+            obj.Roll(1); obj.Roll(4);
+            obj.Roll(7); obj.Roll(3);
+            obj.Roll(2); obj.Roll(2);
+            obj.Roll(1); obj.Roll(1);
+            obj.Roll(10);
+            obj.Roll(2); obj.Roll(3);
+
+            for (int i = 0; i < 21; i++)
+            {
+
+                FinalScore = obj.GetScore();
+            }
+            if (FinalScore != 87)
+            {
+                Assert.Fail("test failed for all spares condition");
+            }
+
+        }
+
+
+        [TestMethod]
+        public void AllSparesWithZeroAndTenArePinsPerFrame()
+        {
+            int FinalScore = 0;
+            var obj = new Bowling.Game();
+            for (int i = 0; i < 11; i++)
+            {
+                obj.Roll(0);
+                obj.Roll(10);
+                FinalScore = obj.GetScore();
+            }
+
+            if (FinalScore != 100)
+            {
+                Assert.Fail("test failed for all spares condition");
+            }
+
+
+            
+
+
+
+        }
+
+
     }
 }
